db7eea3e7249977f86c6b9cbe6080b5b586528d0
AntonioLopezAurelio <antonioaure1997@gmail.com>
2023-09-27 19:02:57 -0600

    add spring security

--- src/main/java/com/apivisorus/service/ICategoria.java
+++ src/main/java/com/apivisorus/service/ICategoria.java
@@ -1,10 +1,7 @@
 package com.apivisorus.service;
 
-
 import java.util.List;
-import java.util.Optional;
 
-import org.springframework.data.jpa.repository.Query;
 
 import com.apivisorus.model.entity.Categoria;
 
